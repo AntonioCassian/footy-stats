f9226a4d8e856416e778bf8735ade08770b64ecc
Antonio Martins <antoniomart849@gmail.com>
2023-08-02 14:05:00 -0300

    feat: create page login

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login">
+        <div class="tit-login">
+            <SubTitle sub="Login" paragraph="Welcome back. Login using your user and password to access amazing features."/>
+        </div>
+    </div>
+</template>
+
+<script>
+import SubTitle from '@/components/template/SubTitle.vue'
+export default {
+    name: 'Login',
+    components: {SubTitle}
+}
+</script>
